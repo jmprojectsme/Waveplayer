ccbe96691b426c5eb0bf68aaea71cb6218152285
Jaymar Reperuga <digicube.projects@gmail.com>
2026-03-23 20:43:22 +0800

    Upgrade cache version and optimize fetch handling

--- sw.js
+++ sw.js
@@ -1,36 +1,35 @@
-const cacheName = 'wave-v5';
+const cacheName = 'wave-v6';
 const assets = [
   './',
   './index.html',
   './style.css',
   './app.js',
-  './manifest.json',
-  './icon.png'
+  './manifest.json'
 ];
 
 self.addEventListener('install', e => {
-  self.skipWaiting(); 
+  self.skipWaiting();
   e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll(assets);
-    })
+    caches.open(cacheName).then(cache => cache.addAll(assets))
   );
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.map(key => {
-          if (key !== cacheName) return caches.delete(key);
-        })
-      );
-    })
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => {
+        if (key !== cacheName) return caches.delete(key);
+      }))
+    )
   );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+    caches.match(e.request)
+      .then(res => res || fetch(e.request)
+        .catch(() => caches.match('./index.html'))
+      )
   );
 });
